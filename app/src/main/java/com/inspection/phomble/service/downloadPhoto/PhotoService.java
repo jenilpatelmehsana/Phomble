42c1b3b90e84e4c9bae299c12d920f89be36f1c3
jenilpatelmehsana <jenilpatelmehsana@gmail.com>
2022-08-26 00:32:48 +0530

    Implemented downloadImage method and made Response object

--- app/src/main/java/com/inspection/phomble/service/downloadPhoto/PhotoService.java
+++ app/src/main/java/com/inspection/phomble/service/downloadPhoto/PhotoService.java
@@ -4,6 +4,8 @@ import static com.inspection.phomble.service.Constants.BASE_API;
 import static com.inspection.phomble.service.Constants.CLIENT_ID;
 import static com.inspection.phomble.service.Constants.RANDOM_EXTENSION;
 
+import com.inspection.phomble.Key;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -60,7 +62,44 @@ public class PhotoService {
         return data;
     }
 
-    public String getRandomPhotoURL() {
-        return BASE_API + RANDOM_EXTENSION + CLIENT_ID;
+    public String downloadImage(String api) throws IOException {
+        api = api + CLIENT_ID + Key.ACCESS_KEY;
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try {
+            URL url = new URL(api);
+
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.connect();
+
+            iStream = httpURLConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            iStream.close();
+            httpURLConnection.disconnect();
+        }
+        return data;
     }
+
+    public String getRandomPhotoURL() {
+        return BASE_API + RANDOM_EXTENSION;
+    }
+
 }
